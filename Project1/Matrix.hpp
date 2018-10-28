f50a52887a605c14854aa94f4458f827e76aecf2
imworstcoder <44238688+imworstcoder@users.noreply.github.com>
2018-10-28 11:27:42 +0200

    Adaugarea operatorilor -,-=

--- Project1/Matrix.hpp
+++ Project1/Matrix.hpp
@@ -1,25 +1,37 @@
 #include <iostream>
 class Matrix{
-	int **m_values;
-	int m_rows;
-	int m_columns;
 public:
-	int Get_rows()
-	{
-		return m_rows;
-	}
-	int Get_columns()
-	{
-		return m_columns;
-	}
 	Matrix(int rows, int columns);
 	Matrix(const Matrix &right);
 	~Matrix();
-	void GetMatrix();
+	int Get_rows(){
+		return m_rows;
+	}
+	int Get_columns(){
+		return m_columns;
+	}
+	void ShowMatrix();
 	void SetMatrixValues();
 	Matrix& operator=(const Matrix& right);
 	friend Matrix operator+(const Matrix &left, const Matrix &right);
+	friend Matrix operator+(const Matrix &left, double scalar);
+	friend Matrix operator+(double scalar, const Matrix &right);
+	friend Matrix operator-(const Matrix &left, const Matrix &right);
+	friend Matrix operator-(const Matrix &left, double scalar);
+	friend Matrix operator-(double scalar, const Matrix &right);
+	/*friend Matrix operator*(const Matrix &left, const Matrix &right);
+	friend Matrix operator*(const Matrix &left, double scalar);
+	friend Matrix operator*(double scalar, const Matrix &right);
+	Matrix& operator*=(const Matrix &right);
+	Matrix& operator*=(double scalar);*/
 	Matrix& operator+=(const Matrix &right);
+	Matrix& operator+=(double scalar);
+	Matrix& operator-=(const Matrix &right);
+	Matrix& operator-=(double scalar);
 
+private:
+	int **m_values;
+	int m_rows;
+	int m_columns;
 	
 };
--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -8,33 +8,6 @@ Matrix:: Matrix(int rows=0, int columns=0) : m_rows(rows), m_columns(columns)
 		m_values[i] = new int[m_columns];
 }
 
-Matrix& Matrix::operator=(const Matrix& right) {
-	if (&right == this) {
-		return *this;
-	}
-	if (m_rows*m_columns == right.m_rows*right.m_columns) {
-		for (int i = 0; i < m_rows; i++)
-			for (int j = 0; j < m_columns; j++)
-				this->m_values[i][j] = right.m_values[i][j];
-	}
-	else {
-		for (int i = 0; i < m_rows; i++)
-			delete[] m_values[i];
-		delete[] m_values;
-		this->m_rows = right.m_rows;
-		this->m_columns = right.m_columns;
-		m_values = new int*[m_rows];
-		for (int i = 0; i < m_rows; i++)
-			m_values[i] = new int[m_columns];
-		for (int i = 0; i < m_rows; i++)
-			for (int j = 0; j < m_columns; j++)
-				m_values[i][j] = right.m_values[i][j];
-	}
-	return *this;
-}
-
-
-
 void Matrix:: SetMatrixValues()
 {
 	int i, j;
@@ -60,7 +33,7 @@ Matrix:: Matrix(const Matrix &right) : m_rows(right.m_rows), m_columns(right.m_c
 			m_values[i][j] = right.m_values[i][j];
 }
 
-void Matrix::GetMatrix()
+void Matrix::ShowMatrix()
 {
 	for (int i = 0; i < m_rows; i++)
 	{
@@ -69,6 +42,31 @@ void Matrix::GetMatrix()
 		std::cout << '\n';
 	}
 	std::cout << '\n';
+}
+
+Matrix& Matrix::operator=(const Matrix& right) {
+	if (&right == this) {
+		return *this;
+	}
+	if (m_rows*m_columns == right.m_rows*right.m_columns) {
+		for (int i = 0; i < m_rows; i++)
+			for (int j = 0; j < m_columns; j++)
+				this->m_values[i][j] = right.m_values[i][j];
+	}
+	else {
+		for (int i = 0; i < m_rows; i++)
+			delete[] m_values[i];
+		delete[] m_values;
+		this->m_rows = right.m_rows;
+		this->m_columns = right.m_columns;
+		m_values = new int*[m_rows];
+		for (int i = 0; i < m_rows; i++)
+			m_values[i] = new int[m_columns];
+		for (int i = 0; i < m_rows; i++)
+			for (int j = 0; j < m_columns; j++)
+				m_values[i][j] = right.m_values[i][j];
+	}
+	return *this;
 }
 
 Matrix operator+(const Matrix &left, const Matrix &right)
@@ -82,6 +80,52 @@ Matrix operator+(const Matrix &left, const Matrix &right)
 	return result;
 }
 
+Matrix operator+(const Matrix &left, double scalar)
+{
+	Matrix result(left.m_rows, left.m_columns);
+	for (int i = 0; i < result.m_rows; i++)
+		for (int j = 0; j < result.m_columns; j++)
+			result.m_values[i][j] = left.m_values[i][j] + scalar;
+	return result;
+}
+
+Matrix operator+(double scalar,const Matrix &right)
+{
+	Matrix result(right.m_rows, right.m_columns);
+	for (int i = 0; i < result.m_rows; i++)
+		for (int j = 0; j < result.m_columns; j++)
+			result.m_values[i][j] = right.m_values[i][j] + scalar;
+	return result;
+}
+
+Matrix operator-(const Matrix &left, const Matrix &right)
+{
+	Matrix result(left.m_rows, left.m_columns);
+	for (int i = 0; i < result.m_rows; i++)
+		for (int j = 0; j < result.m_columns; j++)
+			result.m_values[i][j] = left.m_values[i][j] - right.m_values[i][j];
+	return result;
+}
+
+Matrix operator-(const Matrix &left, double scalar)
+{
+	Matrix result(left.m_rows, left.m_columns);
+	for (int i = 0; i < result.m_rows; i++)
+		for (int j = 0; j < result.m_columns; j++)
+			result.m_values[i][j] = left.m_values[i][j] - scalar;
+	return result;
+}
+
+Matrix operator-(double scalar, const Matrix &right)
+{
+	Matrix result(right.m_rows, right.m_columns);
+	for (int i = 0; i < result.m_rows; i++)
+		for (int j = 0; j < result.m_columns; j++)
+			result.m_values[i][j] = right.m_values[i][j] - scalar;
+	return result;
+}
+
+
 
 Matrix &Matrix::operator+=(const Matrix &right)
 {
@@ -91,16 +135,40 @@ Matrix &Matrix::operator+=(const Matrix &right)
 	return *this;
 }
 
+Matrix &Matrix::operator+=(double scalar)
+{
+	Matrix result;
+	result = (*this) + scalar;
+	(*this) = result;
+	return *this;
+}
+
+Matrix &Matrix::operator-=(const Matrix &right)
+{
+	Matrix created;
+	created = (*this) - right;
+	(*this) = created;
+	return *this;
+}
+
+Matrix &Matrix::operator-=(double scalar)
+{
+	Matrix result;
+	result = (*this) - scalar;
+	(*this) = result;
+	return *this;
+}
+
 int main()
 {
 	Matrix M1(2,2);
-	M1.SetMatrixValues();
-	M1.GetMatrix();
-	Matrix M2(2,2);
+	//M1.SetMatrixValues();
+	//M1.ShowMatrix();
+	Matrix M2(2,2),M3;
 	M2.SetMatrixValues();
-	M2.GetMatrix();
-	M2 += M1;
-	M2.GetMatrix();
+	M2.ShowMatrix();
+	M3 = M2-2;
+	M3.ShowMatrix();
 	system("pause");
 	return 0;
 
